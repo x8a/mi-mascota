c01abfb7c811fa0e21cf2fe4df0496cfdaa6050f
Alba Olveros <x8a.018@gmail.com>
2020-06-16 21:26:27 +0200

    Added pet profile
    Edited user profile
    Edited cookie's expiry date

--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -1,19 +1,20 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const mongoose = require("mongoose");
 const router = new Router();
 const User = require("../models/User.model");
 const Pet = require("../models/Pet.model");
+const { db } = require("../models/User.model");
 
+router.get("/user-profile/addPet", (req, res, next) => {
+  res.render("user/createPet", { userInSession: req.session.currentUser });
+});
 
-router.get('/user-profile/addPet', (req, res, next) => {
-  res.render('user/createPet', {userInSession: req.session.currentUser});
-})
-
-router.post('/user-profile/addPet', async (req, res, next) => {
-  console.table(req.body)
+router.post("/user-profile/addPet", async (req, res, next) => {
+  console.table(req.body);
   const owner = req.session.currentUser._id;
-  const {name, animal, breed, birthdate, age} = req.body;
-  console.log(animal)
+  const { name, animal, breed, birthdate, age } = req.body;
+  console.log(animal);
+  const myPets = await Pet.find({ owner: owner });
   try {
     const petToInsert = new Pet({
       name,
@@ -21,20 +22,33 @@ router.post('/user-profile/addPet', async (req, res, next) => {
       animal,
       breed,
       birthdate,
-      age
-    })
-    const pet = await petToInsert.save()
-    res.redirect('/user-profile');
+      age,
+    });
+    const pet = await petToInsert.save();
+    myPets.push(pet);
+    const user = await User.findOneAndUpdate({ _id: owner }, { pets: myPets });
+    res.redirect("/user-profile");
   } catch (error) {
-    if (error instanceof mongoose.Error.ValidationError){
-      res.status(500).render('user/createPets', {
+    if (error instanceof mongoose.Error.ValidationError) {
+      res.status(500).render("user/createPets", {
         errorMessage: `We couldn't add your pet. Please check that all fields are correct.`,
         userInSession: req.session.currentUser,
-      })
+      });
     } else {
-      next(error)
+      next(error);
     }
   }
-})
+});
 
-module.exports = router
+router.get("/pet-profile/:petId", async (req, res, next) => {
+  try {
+    const pet = await Pet.find({ _id: req.params.petId});
+    res.render("pet/petDetails", {pet});
+    console.log(pet)
+    console.log(pet[0].name)
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Pet = require("../models/Pet.model");
 
-router.get("/user-profile", (req, res) => {
-  res.render("user/profile", {
-    userInSession: req.session.currentUser,
-  });
+router.get("/user-profile", async (req, res, next) => {
+  try {
+    const myPets = await Pet.find({ owner: req.session.currentUser._id });
+    res.render("user/profile", {
+      userInSession: req.session.currentUser,
+      myPets: myPets,
+    });
+  } catch (e) {
+    next(e);
+  }
 });
-
 
 module.exports = router;
