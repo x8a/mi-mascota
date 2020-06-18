f2dab6b8c6048d8f2e6b85466aa92171666edf4b
Alba Olveros <x8a.018@gmail.com>
2020-06-18 11:09:41 +0200

    Added user profile edit route
    Added CSS file
    Created user profile edit view

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User.model");
 const Pet = require("../models/Pet.model");
 
 router.get("/user-profile", async (req, res, next) => {
@@ -7,11 +8,28 @@ router.get("/user-profile", async (req, res, next) => {
     const myPets = await Pet.find({ owner: req.session.currentUser._id });
     res.render("user/profile", {
       userInSession: req.session.currentUser,
-      myPets: myPets,
+      myPets: myPets
     });
   } catch (e) {
     next(e);
   }
 });
+
+router.get("/edit/user-profile/:userId", async (req, res, next) => {
+  try{
+    res.render("user/editProfile", {userInSession: req.session.currentUser});
+  } catch (e) {
+    next(e);
+  }
+})
+
+router.post("/edit/user-profile/:userId", async (req, res, next) => {
+  try{
+    const updatedUser = await User.findByIdAndUpdate(req.body._id, req.body);
+    res.redirect("/user-profile");
+  } catch (e) {
+    next(e);
+  }
+})
 
 module.exports = router;
