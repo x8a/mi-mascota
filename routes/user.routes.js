8c4adc4f0c06902dd23874f3268228385187525f
Alba Olveros <x8a.018@gmail.com>
2020-06-18 11:58:09 +0200

    User password edit

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const User = require("../models/User.model");
 const Pet = require("../models/Pet.model");
 
@@ -15,7 +16,7 @@ router.get("/user-profile", async (req, res, next) => {
   }
 });
 
-router.get("/edit/user-profile/:userId", async (req, res, next) => {
+router.get("/edit/user-profile/:userId", (req, res, next) => {
   try{
     res.render("user/editProfile", {userInSession: req.session.currentUser});
   } catch (e) {
@@ -25,8 +26,24 @@ router.get("/edit/user-profile/:userId", async (req, res, next) => {
 
 router.post("/edit/user-profile/:userId", async (req, res, next) => {
   try{
-    const updatedUser = await User.findByIdAndUpdate(req.body._id, req.body);
-    res.redirect("/user-profile");
+    const {password, newPassword} = req.body;
+    const saltRounds = 10; // Cost of generating the hash
+    const salt = bcrypt.genSaltSync(saltRounds); // Key to generate the hash
+
+    const passHash = bcrypt.hashSync(password, salt); // Generate the hash with a password and the salt
+    const newPassHash = bcrypt.hashSync(newPassword, salt); // Generate the hash with a password and the salt
+    const verifyPass = bcrypt.compareSync(passHash, newPassHash); // Compare the two passwords
+
+    if(password && newPassword && verifyPass) {
+      const updatedUser = await User.findByIdAndUpdate(req.body._id, {passHash: passHash});
+      res.redirect("/user-profile");
+    } else if (password && newPassword && !verifyPass) {
+      res.render("user/editProfile", {errorMessage: "The passwords do not match."});
+    } else {
+      const updatedUser = await User.findByIdAndUpdate(req.body._id, req.body);
+      res.redirect("/user-profile");
+    }
+  
   } catch (e) {
     next(e);
   }
